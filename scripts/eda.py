caee32597439526c2b8edf2c75247ab89c76b074
snazau <snazarikov@gmail.com>
2023-01-22 21:37:25 +0300

    Add distribution visualization of seizure durations across different sets

--- scripts/eda.py
+++ scripts/eda.py
@@ -1,5 +1,9 @@
 import json
 
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy import stats
+
 
 if __name__ == '__main__':
     metainfo_path = '../data/dataset_info.json'
@@ -13,10 +17,25 @@ if __name__ == '__main__':
     total_percent = 0
     avg_percent = 0
 
+    data1_seizure_durations = list()
+    data2_seizure_durations = list()
     for subject_key in dataset_info['subjects_info'].keys():
         subject_seizure_duration = 0
         for seizure in dataset_info['subjects_info'][subject_key]['seizures']:
-            subject_seizure_duration += seizure['end'] - seizure['start']
+            seizure_duration = seizure['end'] - seizure['start']
+
+            if seizure_duration > 300:
+                print('>300', subject_key, seizure_duration, seizure)
+                continue
+
+            subject_seizure_duration += seizure_duration
+
+            if 'data1' in subject_key:
+                data1_seizure_durations.append(seizure_duration)
+            elif 'data2' in subject_key:
+                data2_seizure_durations.append(seizure_duration)
+            else:
+                raise NotImplementedError
 
         subject_normal_duration = dataset_info['subjects_info'][subject_key]['duration_in_seconds'] - subject_seizure_duration
         subject_total_duration = dataset_info['subjects_info'][subject_key]['duration_in_seconds']
@@ -31,3 +50,18 @@ if __name__ == '__main__':
 
     print(f'avg_percent = {avg_percent * 100:.4f}% total_percent = {total_percent * 100:.4f}%')
     print(f'total_normal = {total_normal / 3600:.2f}h total_seizure = {total_seizure / 3600:.2f}h total_duration = {total_duration / 3600:.2f}h')
+
+    data1_seizure_durations = np.array(data1_seizure_durations)
+    data2_seizure_durations = np.array(data2_seizure_durations)
+    print(f'data1_seizure stats min = {data1_seizure_durations.min()} mean = {data1_seizure_durations.mean()} max = {data1_seizure_durations.max()} std = {data1_seizure_durations.std()}')
+    print(f'data2_seizure stats min = {data2_seizure_durations.min()} mean = {data2_seizure_durations.mean()} max = {data2_seizure_durations.max()} std = {data2_seizure_durations.std()}')
+
+    # log_bins = np.logspace(np.log10(50), np.log10(300), 25)
+    # plt.hist([data1_seizure_durations, data2_seizure_durations], bins=log_bins, label=['data1', 'data2'])
+    # plt.gca().set_xscale("log")
+    bins = np.linspace(10, 300, 25)
+    plt.hist([data1_seizure_durations, data2_seizure_durations], bins=bins, label=['data1', 'data2'], density=True)
+    plt.plot(bins, stats.norm.pdf(bins, data1_seizure_durations.mean(), data1_seizure_durations.std()), 'b--')
+    plt.plot(bins, stats.norm.pdf(bins, data2_seizure_durations.mean(), data2_seizure_durations.std()), 'y--')
+    plt.legend(loc='upper right')
+    plt.show()
