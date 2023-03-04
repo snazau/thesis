6a75ec10dcb70fbecabf5cd4334049eee7628a01
snazau <snazarikov@gmail.com>
2023-03-04 17:20:47 +0300

    Add visualization func

--- visualization.py
+++ visualization.py
@@ -1,5 +1,29 @@
+import cv2
 import matplotlib.pyplot as plt
 import numpy as np
+
+
+def create_black_bar_w_text(bar_shape, text, text_location):
+    bar = np.zeros(bar_shape, np.float64)
+
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    fontScale = 1
+    fontColor = (1, 1, 1)
+    thickness = 2
+    lineType = 2
+
+    cv2.putText(
+        bar,
+        text,
+        text_location,
+        font,
+        fontScale,
+        fontColor,
+        thickness,
+        lineType
+    )
+
+    return bar
 
 
 def plot_hist(power_spectrum):
@@ -18,6 +42,42 @@ def plot_spectrum_channels(power_spectrum, time_idx_from, time_idx_to, channel_n
         axes[i % 13, i // 13].imshow((temp - temp.min()) / (temp.max() - temp.min()))
         axes[i % 13, i // 13].set_title(channel_names[i] if channel_names is not None else '')
     plt.show()
+
+
+def visualize_spectrum_channels(power_spectrum, channel_names):
+    # power_spectrum.shape = (C, F, T)
+
+    # vmin = power_spectrum.min()
+    # vmax = power_spectrum.max()
+    # power_spectrum = (power_spectrum - vmin) / (vmax - vmin)
+    # print(f'vmin = {vmin} vmax = {vmax}')
+
+    row_images = list()
+    column_images = list()
+    for i in range(power_spectrum.shape[0]):
+        power_spectrum_channel = power_spectrum[i]
+        power_spectrum_channel = (power_spectrum_channel - power_spectrum_channel.min()) / (power_spectrum_channel.max() - power_spectrum_channel.min())
+        black_bar = create_black_bar_w_text(bar_shape=(40, power_spectrum_channel.shape[-1]), text=channel_names[i], text_location=(10, 30))
+        power_spectrum_channel_w_name = cv2.vconcat([black_bar, power_spectrum_channel])
+        # plt.imshow(power_spectrum_channel_w_name)
+        # plt.show()
+
+        column_images.append(power_spectrum_channel_w_name)
+        # print(i, len(column_images), power_spectrum_channel.shape)
+        if (i + 1) % 3 == 0:
+            column_image = cv2.hconcat(column_images)
+            row_images.append(column_image)
+            column_images = list()
+        elif i == (power_spectrum.shape[0] - 1):
+            for _ in range(3 - len(column_images)):
+                column_images.append(np.zeros_like(column_images[0]))
+            column_image = cv2.hconcat(column_images)
+            row_images.append(column_image)
+            column_images = list()
+    merged_image = cv2.vconcat(row_images)
+    # plt.imshow(merged_image, cmap='gray')
+    # plt.show()
+    return merged_image
 
 
 def plot_spectrum_averaged(power_spectrum, freqs):
