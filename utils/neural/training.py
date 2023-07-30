508ff3f5bb6074409cf94e82a9d32d48e7a355c2
snazau <snazarikov@gmail.com>
2023-07-30 15:29:17 +0300

    Add efficientnet_b0 model

--- utils/neural/training.py
+++ utils/neural/training.py
@@ -50,6 +50,20 @@ def get_model(model_name, model_kwargs):
             torch.nn.ReLU(),
             torch.nn.Linear(64, 1),
         )
+    elif model_name == 'efficientnet_b0':
+        import timm
+        model = timm.create_model('efficientnet_b0.ra_in1k', pretrained=True)
+
+        conv_stem_pretrained_weight = model.conv_stem.weight
+        model.conv_stem = torch.nn.Conv2d(25, 64, kernel_size=3, stride=2, padding=1, bias=False)
+        conv_stem_weight = torch.cat([torch.mean(conv_stem_pretrained_weight, dim=1, keepdim=True) for _ in range(25)], dim=1)
+        model.conv_stem.weight = torch.nn.parameter.Parameter(conv_stem_weight, requires_grad=True)
+
+        model.classifier = torch.nn.Sequential(
+            torch.nn.Linear(1280, 64),
+            torch.nn.ReLU(),
+            torch.nn.Linear(64, 1),
+        )
     else:
         raise NotImplementedError
 
