1499b2e7ab0480800e04501fa651c5feaef690fe
snazau <snazarikov@gmail.com>
2023-08-09 21:25:28 +0300

    Update ResNet18 and EfficientNet-b0 models

--- models/efficientnet.py
+++ models/efficientnet.py
@@ -3,19 +3,42 @@ import timm
 
 
 class EEGEfficientNetB0Spectrum(torch.nn.Module):
-    def __init__(self, pretrained):
+    def __init__(self, pretrained=False):
         super().__init__()
 
         self.pretrained = pretrained
-        model = timm.create_model('efficientnet_b0.ra_in1k', pretrained=self.pretrained)
+        self.model = timm.create_model('efficientnet_b0.ra_in1k', pretrained=self.pretrained)
 
-        conv_stem_pretrained_weight = model.conv_stem.weight
-        model.conv_stem = torch.nn.Conv2d(25, 64, kernel_size=3, stride=2, padding=1, bias=False)
+        conv_stem_pretrained_weight = self.model.conv_stem.weight
+        self.model.conv_stem = torch.nn.Conv2d(25, 64, kernel_size=3, stride=2, padding=1, bias=False)
         if self.pretrained:
             conv_stem_weight = torch.cat([torch.mean(conv_stem_pretrained_weight, dim=1, keepdim=True) for _ in range(25)], dim=1)
-            model.conv_stem.weight = torch.nn.parameter.Parameter(conv_stem_weight, requires_grad=True)
+            self.model.conv_stem.weight = torch.nn.parameter.Parameter(conv_stem_weight, requires_grad=True)
 
-        model.classifier = torch.nn.Sequential(
+        self.model.classifier = torch.nn.Sequential(
+            torch.nn.Linear(1280, 64),
+            torch.nn.ReLU(),
+            torch.nn.Linear(64, 1),
+        )
+
+    def forward(self, x):
+        return self.model(x)
+
+
+class EEGEfficientNetB0Raw(torch.nn.Module):
+    def __init__(self, pretrained=False):
+        super().__init__()
+
+        self.pretrained = pretrained
+        self.model = timm.create_model('efficientnet_b0.ra_in1k', pretrained=self.pretrained)
+
+        conv_stem_pretrained_weight = self.model.conv_stem.weight
+        self.model.conv_stem = torch.nn.Conv2d(1, 64, kernel_size=3, stride=2, padding=1, bias=False)
+        if self.pretrained:
+            conv_stem_weight = torch.mean(conv_stem_pretrained_weight, dim=1, keepdim=True)
+            self.model.conv_stem.weight = torch.nn.parameter.Parameter(conv_stem_weight, requires_grad=True)
+
+        self.model.classifier = torch.nn.Sequential(
             torch.nn.Linear(1280, 64),
             torch.nn.ReLU(),
             torch.nn.Linear(64, 1),
--- models/resnet.py
+++ models/resnet.py
@@ -3,19 +3,19 @@ import torchvision
 
 
 class EEGResNet18Spectrum(torch.nn.Module):
-    def __init__(self, pretrained):
+    def __init__(self, pretrained=False):
         super().__init__()
 
         self.pretrained = pretrained
-        model = torchvision.models.resnet18(pretrained=self.pretrained)
+        self.model = torchvision.models.resnet18(pretrained=self.pretrained)
 
-        conv1_pretrained_weight = model.conv1.weight
-        model.conv1 = torch.nn.Conv2d(25, 64, kernel_size=7, stride=2, padding=3, bias=False)
+        conv1_pretrained_weight = self.model.conv1.weight
+        self.model.conv1 = torch.nn.Conv2d(25, 64, kernel_size=7, stride=2, padding=3, bias=False)
         if self.pretrained:
             conv1_weight = torch.cat([torch.mean(conv1_pretrained_weight, dim=1, keepdim=True) for _ in range(25)], dim=1)
-            model.conv1.weight = torch.nn.parameter.Parameter(conv1_weight, requires_grad=True)
+            self.model.conv1.weight = torch.nn.parameter.Parameter(conv1_weight, requires_grad=True)
 
-        model.fc = torch.nn.Sequential(
+        self.model.fc = torch.nn.Sequential(
             torch.nn.Linear(512, 64),
             torch.nn.ReLU(),
             torch.nn.Linear(64, 1),
@@ -26,19 +26,19 @@ class EEGResNet18Spectrum(torch.nn.Module):
 
 
 class EEGResNet18Raw(torch.nn.Module):
-    def __init__(self, pretrained):
+    def __init__(self, pretrained=False):
         super().__init__()
 
         self.pretrained = pretrained
-        model = torchvision.models.resnet18(pretrained=self.pretrained)
+        self.model = torchvision.models.resnet18(pretrained=self.pretrained)
 
-        conv1_pretrained_weight = model.conv1.weight
-        model.conv1 = torch.nn.Conv2d(1, 64, kernel_size=7, stride=2, padding=3, bias=False)
+        conv1_pretrained_weight = self.model.conv1.weight
+        self.model.conv1 = torch.nn.Conv2d(1, 64, kernel_size=7, stride=2, padding=3, bias=False)
         if self.pretrained:
             conv1_weight = torch.mean(conv1_pretrained_weight, dim=1, keepdim=True)
-            model.conv1.weight = torch.nn.parameter.Parameter(conv1_weight, requires_grad=True)
+            self.model.conv1.weight = torch.nn.parameter.Parameter(conv1_weight, requires_grad=True)
 
-        model.fc = torch.nn.Sequential(
+        self.model.fc = torch.nn.Sequential(
             torch.nn.Linear(512, 64),
             torch.nn.ReLU(),
             torch.nn.Linear(64, 1),
