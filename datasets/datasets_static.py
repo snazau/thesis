a5c80a9a4384acef40414e17fff412cc528498c6
snazau <snazarikov@gmail.com>
2023-12-21 23:24:31 +0300

    - datasets_static.SubjectRandomDataset now supports mode when it takes samples only from provided positive predictions - disabled filtering for provided predictions - add random noise to samples from provided predictions

--- datasets/datasets_static.py
+++ datasets/datasets_static.py
@@ -91,6 +91,17 @@ def drop_unused_channels(eeg_file_path, raw_file):
     raw_file.drop_channels(channels_to_drop)
 
 
+def extract_tp_times(prediction_data, threshold, sfreq):
+    time_idxs_start = prediction_data['time_idxs_start']
+    probs = prediction_data['probs_wo_tta']
+    labels = prediction_data['labels']
+
+    tp_mask = ((labels == 1) & (probs > threshold))
+    tp_start_times = time_idxs_start[tp_mask] / sfreq
+
+    return tp_start_times
+
+
 def extract_fp_times(prediction_data, threshold, sfreq):
     time_idxs_start = prediction_data['time_idxs_start']
     probs = prediction_data['probs_wo_tta']
@@ -192,6 +203,7 @@ class SubjectRandomDataset(torch.utils.data.Dataset):
             seizures,
             samples_num,
             prediction_data_path=None,
+            prediction_data_only_positives=False,
             stats_path=None,
             sample_duration=60,
             normal_samples_fraction=0.5,
@@ -206,6 +218,7 @@ class SubjectRandomDataset(torch.utils.data.Dataset):
         self.seizures = seizures
         self.samples_num = samples_num
         self.prediction_data = None if prediction_data_path is None else pickle.load(open(prediction_data_path, 'rb'))
+        self.prediction_data_only_positives = prediction_data_only_positives
         self.stats_data = None if stats_path is None else pickle.load(open(stats_path, 'rb'))
         self.sample_duration = sample_duration
         self.data_type = data_type
@@ -214,6 +227,9 @@ class SubjectRandomDataset(torch.utils.data.Dataset):
         self.baseline_correction = baseline_correction
         self.force_calc_baseline_stats = force_calc_baseline_stats
         self.transform = transform
+
+        if self.prediction_data_only_positives is True:
+            assert self.prediction_data is not None, 'In case you want to train only on positives from prediction data you need to have this prediction data'
 
         # trim last seizure
         if self.seizures[-1]['end'] + self.sample_duration >= self.raw.times.max():
@@ -372,70 +388,127 @@ class SubjectRandomDataset(torch.utils.data.Dataset):
         return sample
 
     def _generate_data(self):
-        # get start time for samples
-        seizure_times = custom_distribution.unifrom_segments_sample(size=self.samples_num, segments=self.seizures)
-        assert all(
-            [
-                any([seizure['start'] < seizure_time < seizure['end'] for seizure in self.seizures])
-                for seizure_time in seizure_times
+        if not self.prediction_data_only_positives:
+            # get start time for samples
+            seizure_times = custom_distribution.unifrom_segments_sample(size=self.samples_num, segments=self.seizures)
+            assert all(
+                [
+                    any([seizure['start'] < seizure_time < seizure['end'] for seizure in self.seizures])
+                    for seizure_time in seizure_times
+                ]
+            )
+
+            normal_times = custom_distribution.unifrom_segments_sample(size=self.samples_num, segments=self.normal_segments)
+            assert all(
+                [
+                    any(
+                        [normal_segment['start'] < normal_time < normal_segment['end'] for normal_segment in
+                         self.normal_segments]
+                    )
+                    for normal_time in normal_times
+                ]
+            )
+
+            # generate samples
+            mask = np.random.uniform(size=self.samples_num) > self.normal_samples_fraction
+            targets = mask.astype(int)
+            sample_start_times = mask * seizure_times + (1 - mask) * normal_times
+
+            if self.prediction_data is not None:
+                fp_start_times = extract_fp_times(self.prediction_data, threshold=0.70, sfreq=self.raw.info['sfreq'])
+                fn_start_times = extract_fn_times(self.prediction_data, threshold=0.99, sfreq=self.raw.info['sfreq'])
+
+                # fp_start_times = filter_fp_times(fp_start_times, self.seizures, min_deviation=300, min_start_time=900)
+                # fn_start_times = filter_fn_times(fn_start_times, self.seizures, min_deviation=-1, min_start_time=900)
+
+                fn_num_to_pick = min(len(fn_start_times), self.samples_num // 2)
+                fp_num_to_pick = min(len(fp_start_times), self.samples_num // 2)
+                # fp_num_to_pick = min(len(fp_start_times), self.samples_num // 2, fn_num_to_pick)
+
+                fp_picked_start_times = np.random.choice(fp_start_times, size=fp_num_to_pick, replace=False)
+                fn_picked_start_times = np.random.choice(fn_start_times, size=fn_num_to_pick, replace=False)
+
+                false_preds_start_times = np.append(fp_picked_start_times, fn_picked_start_times)
+                false_preds_labels = np.append(np.zeros_like(fp_picked_start_times), np.ones_like(fn_picked_start_times))
+
+                random_permutation = np.random.permutation(len(false_preds_labels))
+                false_preds_start_times = false_preds_start_times[random_permutation]
+                false_preds_labels = false_preds_labels[random_permutation]
+
+                random_noise_in_seconds = (np.random.randint(0, high=11, size=false_preds_start_times.shape) / 5) - 1
+                false_preds_start_times += random_noise_in_seconds
+                false_preds_start_times = np.clip(false_preds_start_times, a_min=0, a_max=self.time_end - self.sample_duration - 1e-3)
+
+                sample_start_times = np.append(sample_start_times, false_preds_start_times)
+                targets = np.append(targets, false_preds_labels)
+
+                # sample_start_times = sample_start_times[-self.samples_num:]
+                # targets = targets[-self.samples_num:]
+
+                random_permutation = np.random.permutation(len(sample_start_times))
+                sample_start_times = sample_start_times[random_permutation]
+                targets = targets[random_permutation]
+
+            # get time to closest seizure
+            times_to_closest_seizure = [
+                calc_signed_time_to_closest_seizure(sample_start_time, self.seizures)
+                for sample_start_time in sample_start_times
             ]
-        )
 
-        normal_times = custom_distribution.unifrom_segments_sample(size=self.samples_num, segments=self.normal_segments)
-        assert all(
-            [
-                any(
-                    [normal_segment['start'] < normal_time < normal_segment['end'] for normal_segment in
-                     self.normal_segments]
-                )
-                for normal_time in normal_times
-            ]
-        )
+            raw_samples, time_idxs_start, time_idxs_end = generate_raw_samples(self.raw, sample_start_times, self.sample_duration)
 
-        # generate samples
-        mask = np.random.uniform(size=self.samples_num) > self.normal_samples_fraction
-        targets = mask.astype(int)
-        sample_start_times = mask * seizure_times + (1 - mask) * normal_times
+            return mask, sample_start_times, targets, raw_samples, time_idxs_start, time_idxs_end, times_to_closest_seizure
+        else:
+            assert self.prediction_data is not None, 'In case you want to train only on positives from prediction data you need to have this prediction data'
 
-        if self.prediction_data is not None:
-            fp_start_times = extract_fp_times(self.prediction_data, threshold=0.70, sfreq=self.raw.info['sfreq'])
-            fn_start_times = extract_fn_times(self.prediction_data, threshold=0.99, sfreq=self.raw.info['sfreq'])
-
-            fp_start_times = filter_fp_times(fp_start_times, self.seizures, min_deviation=300, min_start_time=900)
-            fn_start_times = filter_fn_times(fn_start_times, self.seizures, min_deviation=-1, min_start_time=900)
+            fn_start_times = extract_fn_times(self.prediction_data, threshold=0.90, sfreq=self.raw.info['sfreq'])
+            fp_start_times = extract_fp_times(self.prediction_data, threshold=0.90, sfreq=self.raw.info['sfreq'])
+            tp_start_times = extract_tp_times(self.prediction_data, threshold=0.90, sfreq=self.raw.info['sfreq'])
 
             fn_num_to_pick = min(len(fn_start_times), self.samples_num // 2)
-            fp_num_to_pick = min(len(fp_start_times), self.samples_num // 2, fn_num_to_pick)
+            tp_num_to_pick = min(len(tp_start_times), self.samples_num // 2)
+            fp_num_to_pick = min(len(fp_start_times), self.samples_num - tp_num_to_pick)
 
-            fp_picked_start_times = np.random.choice(fp_start_times, size=fp_num_to_pick, replace=False)
             fn_picked_start_times = np.random.choice(fn_start_times, size=fn_num_to_pick, replace=False)
+            fp_picked_start_times = np.random.choice(fp_start_times, size=fp_num_to_pick, replace=False)
+            tp_picked_start_times = np.random.choice(tp_start_times, size=tp_num_to_pick, replace=False)
 
-            false_preds_start_times = np.append(fp_picked_start_times, fn_picked_start_times)
-            false_preds_labels = np.append(np.zeros_like(fp_picked_start_times), np.ones_like(fn_picked_start_times))
+            positive_preds_start_times = np.append(fp_picked_start_times, tp_picked_start_times)
+            positive_preds_targets = np.append(np.zeros_like(fp_picked_start_times), np.ones_like(tp_picked_start_times))
 
-            random_permutation = np.random.permutation(len(false_preds_labels))
-            false_preds_start_times = false_preds_start_times[random_permutation]
-            false_preds_labels = false_preds_labels[random_permutation]
+            positive_preds_start_times = np.append(positive_preds_start_times, fn_picked_start_times)
+            positive_preds_targets = np.append(positive_preds_targets, np.ones_like(fn_picked_start_times))
 
-            sample_start_times = np.append(sample_start_times, false_preds_start_times)
-            targets = np.append(targets, false_preds_labels)
+            random_permutation = np.random.permutation(len(positive_preds_targets))
+            positive_preds_start_times = positive_preds_start_times[random_permutation]
+            positive_preds_targets = positive_preds_targets[random_permutation]
 
-            sample_start_times = sample_start_times[-self.samples_num:]
-            targets = targets[-self.samples_num:]
+            # Add +-1 sec noise
+            # random_noise_in_seconds = (np.random.randint(0, high=11, size=positive_preds_start_times.shape) / 5) - 1
+            random_noise_in_seconds = 2 * np.random.rand(*positive_preds_start_times.shape) - 1
+            positive_preds_start_times += random_noise_in_seconds
+            positive_preds_start_times = np.clip(positive_preds_start_times, a_min=0, a_max=self.time_end - self.sample_duration - 1e-3)
 
-            random_permutation = np.random.permutation(len(sample_start_times))
-            sample_start_times = sample_start_times[random_permutation]
-            targets = targets[random_permutation]
+            sample_start_times = positive_preds_start_times
+            targets = positive_preds_targets
 
-        # get time to closest seizure
-        times_to_closest_seizure = [
-            calc_signed_time_to_closest_seizure(sample_start_time, self.seizures)
-            for sample_start_time in sample_start_times
-        ]
+            # random_permutation = np.random.permutation(len(sample_start_times))
+            # sample_start_times = sample_start_times[random_permutation]
+            # targets = targets[random_permutation]
 
-        raw_samples, time_idxs_start, time_idxs_end = generate_raw_samples(self.raw, sample_start_times, self.sample_duration)
+            times_to_closest_seizure = [
+                calc_signed_time_to_closest_seizure(sample_start_time, self.seizures)
+                for sample_start_time in sample_start_times
+            ]
 
-        return mask, sample_start_times, targets, raw_samples, time_idxs_start, time_idxs_end, times_to_closest_seizure
+            raw_samples, time_idxs_start, time_idxs_end = generate_raw_samples(self.raw, sample_start_times, self.sample_duration)
+
+            # print()
+            # print(self.eeg_file_path, list(sample_start_times))
+            # print()
+            mask = targets.astype(bool)
+
+            return mask, sample_start_times, targets, raw_samples, time_idxs_start, time_idxs_end, times_to_closest_seizure
 
     def renew_data(self):
         (
