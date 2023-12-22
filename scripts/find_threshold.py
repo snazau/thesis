dd48ab90b1e7bb61854b5f427ef8b56ad0b6a8b4
snazau <snazarikov@gmail.com>
2023-12-22 21:22:07 +0300

    Refactored find_threshold.py a bit

--- scripts/find_threshold.py
+++ scripts/find_threshold.py
@@ -7,6 +7,53 @@ import utils.avg_meters
 from utils.common import filter_predictions, calc_metrics
 
 
+def get_metrics(experiment_dir, subject_keys, threshold, filter_method='median', k_size=7, sfreq=128, verbose=0):
+    metric_meter = utils.avg_meters.MetricMeter()
+    for subject_key in subject_keys:
+        if verbose:
+            print(f'\rthreshold = {threshold:3.2f} ', end='')
+
+        try:
+            prediction_path = os.path.join(experiment_dir, 'predictions', rf'{subject_key}.pickle')
+            prediction_data = pickle.load(open(prediction_path, 'rb'))
+        except Exception as e:
+            prediction_path = os.path.join(experiment_dir, 'predictions_positive_only', rf'{subject_key}.pickle')
+            prediction_data = pickle.load(open(prediction_path, 'rb'))
+
+        # get data from prediction_data
+        time_idxs_start = prediction_data['time_idxs_start']
+        time_idxs_end = prediction_data['time_idxs_end']
+        probs = prediction_data['probs_wo_tta'] if 'probs_wo_tta' in prediction_data else prediction_data['probs']
+        labels = prediction_data['labels']
+
+        if len(probs) == 0:
+            probs = prediction_data['probs']
+
+        probs_filtered = filter_predictions(probs, filter_method, k_size)
+
+        # metrics
+        subject_metrics = calc_metrics(probs_filtered, labels, threshold)
+        subject_metrics['duration'] = (time_idxs_end[-1] / sfreq - time_idxs_start[0] / sfreq) / 3600
+
+        metric_meter.update(subject_metrics)
+
+        if verbose > 1:
+            print(
+                f'{subject_key:60}'
+                f'threshold = {threshold:3.2f} '
+                f'p = {subject_metrics["precision_score"]:10.4f} ({metric_meter.meters["precision_score"].avg:10.4f}) '
+                f'r = {subject_metrics["recall_score"]:10.4f} ({metric_meter.meters["recall_score"].avg:10.4f}) '
+                f'f1 = {subject_metrics["f1_score"]:10.4f} ({metric_meter.meters["f1_score"].avg:10.4f}) '
+                f'tp = {subject_metrics["tp_num"]:6} ({metric_meter.meters["tp_num"].avg:6.2f}) '
+                f'tn = {subject_metrics["tn_num"]:6} ({metric_meter.meters["tn_num"].avg:6.2f}) '
+                f'fp = {subject_metrics["fp_num"]:6} ({metric_meter.meters["fp_num"].avg:6.2f}) '
+                # f'fn = {subject_metrics["fn_num"]:6} ({metric_meter.meters["fn_num"].avg:6.2f}) '
+                f'duration = {subject_metrics["duration"]:6.2f}'
+            )
+
+    return metric_meter
+
+
 def get_best_threshold(experiment_dir, subject_keys, filter_method='median', k_size=7, sfreq=128, verbose=0):
     assert filter_method in ['mean', 'median', None]
 
@@ -15,48 +62,7 @@ def get_best_threshold(experiment_dir, subject_keys, filter_method='median', k_s
     best_metric_meter = None
     threshold_range = list(np.round(np.arange(0.1, 1, 0.05), 2))
     for threshold_idx, threshold in enumerate(threshold_range):
-        metric_meter = utils.avg_meters.MetricMeter()
-        for subject_key in subject_keys:
-            if verbose:
-                print(f'\rthreshold = {threshold:3.2f} Progress {threshold_idx + 1}/{len(threshold_range)} ', end='')
-
-            try:
-                prediction_path = os.path.join(experiment_dir, 'predictions', rf'{subject_key}.pickle')
-                prediction_data = pickle.load(open(prediction_path, 'rb'))
-            except Exception as e:
-                prediction_path = os.path.join(experiment_dir, 'predictions_positive_only', rf'{subject_key}.pickle')
-                prediction_data = pickle.load(open(prediction_path, 'rb'))
-
-            # get data from prediction_data
-            time_idxs_start = prediction_data['time_idxs_start']
-            time_idxs_end = prediction_data['time_idxs_end']
-            probs = prediction_data['probs_wo_tta'] if 'probs_wo_tta' in prediction_data else prediction_data['probs']
-            labels = prediction_data['labels']
-
-            if len(probs) == 0:
-                probs = prediction_data['probs']
-
-            probs_filtered = filter_predictions(probs, filter_method, k_size)
-
-            # metrics
-            subject_metrics = calc_metrics(probs_filtered, labels, threshold)
-            subject_metrics['duration'] = (time_idxs_end[-1] / sfreq - time_idxs_start[0] / sfreq) / 3600
-
-            metric_meter.update(subject_metrics)
-
-            if verbose > 1:
-                print(
-                    f'{subject_key:60}'
-                    f'threshold = {threshold:3.2f} '
-                    f'p = {subject_metrics["precision_score"]:10.4f} ({metric_meter.meters["precision_score"].avg:10.4f}) '
-                    # f'r = {subject_metrics["recall_score"]:10.4f} ({metric_meter.meters["recall_score"].avg:10.4f}) '
-                    # f'f1 = {subject_metrics["f1_score"]:10.4f} ({metric_meter.meters["f1_score"].avg:10.4f}) '
-                    f'tp = {subject_metrics["tp_num"]:6} ({metric_meter.meters["tp_num"].avg:6.2f}) '
-                    f'tn = {subject_metrics["tn_num"]:6} ({metric_meter.meters["tn_num"].avg:6.2f}) '
-                    f'fp = {subject_metrics["fp_num"]:6} ({metric_meter.meters["fp_num"].avg:6.2f}) '
-                    # f'fn = {subject_metrics["fn_num"]:6} ({metric_meter.meters["fn_num"].avg:6.2f}) '
-                    f'duration = {subject_metrics["duration"]:6.2f}'
-                )
+        metric_meter = get_metrics(experiment_dir, subject_keys, threshold, filter_method, k_size, sfreq, verbose)
 
         if metric_meter.meters['f1_score'].avg > best_avg_f1:
             best_avg_f1 = metric_meter.meters['f1_score'].avg
@@ -64,7 +70,7 @@ def get_best_threshold(experiment_dir, subject_keys, filter_method='median', k_s
             best_metric_meter = metric_meter
 
         if verbose:
-            print(f'threshold = {threshold:3.2f} f1_score = {metric_meter.meters["f1_score"].avg:.4f} best_threshold = {best_threshold:3.2f} best_f1_score = {best_metric_meter.meters["f1_score"].avg:.4f}')
+            print(f'threshold = {threshold:3.2f} f1_score = {metric_meter.meters["f1_score"].avg:.4f} precision_score = {metric_meter.meters["precision_score"].avg:.4f} recall_score = {metric_meter.meters["recall_score"].avg:.4f} best_threshold = {best_threshold:3.2f} best_f1_score = {best_metric_meter.meters["f1_score"].avg:.4f}')
 
     return best_threshold, best_metric_meter
 
@@ -143,6 +149,3 @@ if __name__ == '__main__':
         k_size,
     )
     print(f'\nbest_threshold = {best_threshold} best_metric_meter:\n{best_metric_meter}')
-
-
-
