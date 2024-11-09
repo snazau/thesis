bf18e600dfc9662d85466cd2b55ab73969ab4687
snazau <snazarikov@gmail.com>
2024-11-09 20:19:09 +0300

    Adjusted metrics calculation (for TP it's enough to intersect neighborhood)

--- utils/common.py
+++ utils/common.py
@@ -86,6 +86,7 @@ def calc_segments_metrics(
 
     seizures_true_used_mask = [0 for _ in range(len(seizure_segments_true))]
 
+    fp_flags = list()
     long_postivies = 0
     tp_num, fp_num = 0, 0
     for seizure_segment_pred in seizure_segments_pred:
@@ -107,21 +108,40 @@ def calc_segments_metrics(
                 'start': seizure_segment_true['start'] - seizure_segments_true_dilation,
                 'end': seizure_segment_true['end'] + seizure_segments_true_dilation,
             }
-            if (seizure_segment_true_dilated['start'] <= seizure_segment_pred['end'] <= seizure_segment_true_dilated['end']) and \
-                    (seizure_segment_true_dilated['start'] <= seizure_segment_pred['start'] <= seizure_segment_true_dilated['end']):
+            overlapping_distance = overlapping_segment(seizure_segment_true_dilated, seizure_segment_pred)
+            pred_distance = seizure_segment_pred['end'] - seizure_segment_pred['start']
+            intersection_part = overlapping_distance / pred_distance
+            long_postivies += 1 if pred_distance > 300 else 0
+
+            tp_condition_1 = (seizure_segment_true_dilated['start'] <= seizure_segment_pred['end'] <= seizure_segment_true_dilated['end']) and \
+                             (seizure_segment_true_dilated['start'] <= seizure_segment_pred['start'] <= seizure_segment_true_dilated['end'])
+            tp_condition_2 = overlapping_distance > 0 and pred_distance <= 300
+
+            # if (seizure_segment_true_dilated['start'] <= seizure_segment_pred['end'] <= seizure_segment_true_dilated['end']) and \
+            #         (seizure_segment_true_dilated['start'] <= seizure_segment_pred['start'] <= seizure_segment_true_dilated['end']):
+            if overlapping_distance > 0:
+            # if tp_condition_1 or tp_condition_2:
                 if seizures_true_used_mask[seizure_true_idx] == 0:
                     tp_num += 1
                 find_tp = True
                 seizures_true_used_mask[seizure_true_idx] += 1
 
                 pred_distance = seizure_segment_pred['end'] - seizure_segment_pred['start']
-                long_postivies += 1 if pred_distance > 300 else 0
+                # long_postivies += 1 if pred_distance > 300 else 0
+                # if pred_distance > 300:
+                #     print('VERY LONG TP')
                 break
 
         if not find_tp:
             fp_num += 1
 
+        fp_flags.append(find_tp)
+
     fn_num = seizures_true_used_mask.count(0)
+
+    # for seizure_idx in range(len(seizures_true_used_mask)):
+    #     if seizures_true_used_mask[seizure_idx] > 1:
+    #         print(f'Multiple ({seizures_true_used_mask[seizure_idx]}) preds for single seizure')
 
     # tn_num = 0
     # for normal_segment_pred in normal_segments_pred:
@@ -159,6 +179,79 @@ def calc_segments_metrics(
     }
 
     return metric_dict
+
+
+def get_tp_fp_fn_segments(
+        seizure_segments_true,
+        normal_segments_true,
+        seizure_segments_pred,
+        normal_segments_pred,
+        intersection_part_threshold,
+        record_duration,  # record duration in hours
+        seizure_segments_true_dilation=60 * 1,  # T param, how much to dilate true seizures
+):
+    assert 0 < intersection_part_threshold <= 1
+
+    tp_segments_info = [dict() for _ in range(len(seizure_segments_true))]
+
+    fp_segments = list()
+    for seizure_segment_pred in seizure_segments_pred:
+        find_tp = False
+        closest_true_segment = None
+        closest_true_segment_dilated = None
+        closest_distance = float('inf')
+        for seizure_true_idx, seizure_segment_true in enumerate(seizure_segments_true):
+            seizure_segment_true_dilated = {
+                'start': seizure_segment_true['start'] - seizure_segments_true_dilation,
+                'end': seizure_segment_true['end'] + seizure_segments_true_dilation,
+            }
+
+            distance = min(
+                abs(seizure_segment_pred['start'] - seizure_segment_true['start']),
+                abs(seizure_segment_pred['start'] - seizure_segment_true['end']),
+                abs(seizure_segment_pred['end'] - seizure_segment_true['start']),
+                abs(seizure_segment_pred['end'] - seizure_segment_true['end']),
+            )
+            if distance < closest_distance:
+                closest_distance = distance
+                closest_true_segment = seizure_segment_true
+                closest_true_segment_dilated = seizure_segment_true_dilated
+
+            if (seizure_segment_true_dilated['start'] <= seizure_segment_pred['end'] <= seizure_segment_true_dilated['end']) or \
+                    (seizure_segment_true_dilated['start'] <= seizure_segment_pred['start'] <= seizure_segment_true_dilated['end']):
+
+                if 'preds' not in tp_segments_info[seizure_true_idx]:
+                    tp_segments_info[seizure_true_idx]['preds'] = list()
+
+                tp_segments_info[seizure_true_idx]['preds'].append(seizure_segment_pred)
+                tp_segments_info[seizure_true_idx]['true'] = seizure_segment_true
+                tp_segments_info[seizure_true_idx]['true_dilated'] = seizure_segment_true_dilated
+
+                find_tp = True
+                break
+
+        if not find_tp:
+            fp_segments.append({
+                'pred': seizure_segment_pred,
+                'true': closest_true_segment,
+                'true_dilated': closest_true_segment_dilated,
+            })
+
+    tp_segments, fn_segments = list(), list()
+    for idx, tp_segment_info in enumerate(tp_segments_info):
+        if len(tp_segment_info.keys()) == 0:
+            fn_segment_info = {
+                'true': seizure_segments_true[idx],
+                'true_dilated': {
+                    'start': seizure_segments_true[idx]['start'] - seizure_segments_true_dilation,
+                    'end': seizure_segments_true[idx]['end'] + seizure_segments_true_dilation,
+                },
+            }
+            fn_segments.append(fn_segment_info)
+        else:
+            tp_segments.append(tp_segment_info)
+
+    return tp_segments, fp_segments, fn_segments
 
 
 def overlapping_segment(segment1, segment2):
