63f51c7b74b984c3e3ea00e421f4b2b6961ca580
snazau <snazarikov@gmail.com>
2024-11-09 20:23:50 +0300

    Add importance calculation to EEGResNet18Spectrum model

--- models/resnet.py
+++ models/resnet.py
@@ -1,6 +1,9 @@
 import torch
 import torchvision
 
+import models.gradcam
+import models.cameras
+
 
 class EEGResNet18Spectrum(torch.nn.Module):
     def __init__(self, pretrained=False, initial_bn=False):
@@ -55,6 +58,63 @@ class EEGResNet18Spectrum(torch.nn.Module):
         if self.initial_bn:
             x = self.bn(x)
         return self.model(x)
+
+    def interpret(self, x, layer='model.layer4.1.conv2', return_fm=False):
+        # x.shape = (B, 25, H, W)
+
+        gcam = models.gradcam.GradCAM(self, candidate_layers=None)
+        gcam.forward(x)
+
+        pred_class_id = torch.tensor(0)
+        gcam.backward(ids=pred_class_id)
+
+        heatmap, fmaps, grads = gcam.generate(target_layer=layer)
+        # heatmap.shape = (B, 1, H, W)
+        # fmaps.shape = (B, 512, H / 32, W / 32)
+        # grads.shape = (B, 512, H / 32, W / 32)
+
+        if return_fm:
+            return heatmap, fmaps, grads
+        else:
+            return heatmap
+
+    def cameras(self, x, layer='model.layer4.1.conv2'):
+        batch_size, channels, freq_dim, time_dim = x.shape[:4]
+
+        input_resolutions = list()
+        curr_res = [freq_dim, time_dim]
+        for i in range(4):
+            input_resolutions.append(tuple(curr_res))
+            curr_res[0] = int(curr_res[0] + 0.1 * freq_dim)
+            curr_res[1] = int(curr_res[1] + 0.1 * time_dim)
+
+        cameras = models.cameras.CAMERAS(
+            model=self,
+            targetLayerName=layer,
+            inputResolutions=input_resolutions
+        )
+        heatmap = cameras.run(x, classOfInterest=0)  # (B, 1, F, T)
+
+        return heatmap
+
+    def channel_importance(self, x):
+        # x.shape = (B, 25, H, W)
+
+        self.zero_grad()
+        # x.requires_grad = True
+        x.requires_grad_()
+
+        preds = self.forward(x)  # (B, 1)
+
+        pred_class_id = torch.tensor(0)
+        grad = torch.zeros_like(preds)
+        grad[:, pred_class_id] = 1
+        preds.backward(gradient=grad, retain_graph=True)
+        grad_x = x.grad  # (B, 25, H, W)
+
+        # importance = torch.sum(grad_x, dim=(2, 3))  # (B, 25)
+
+        return grad_x
 
 
 class EEGResNet18Raw(torch.nn.Module):
@@ -113,5 +173,39 @@ class EEGResNet18Raw(torch.nn.Module):
 
 
 if __name__ == '__main__':
-    model = EEGResNet18Raw(pretrained=False)
+    # test spectrum input
+    model = EEGResNet18Spectrum(pretrained=False, initial_bn=True)
     print(model)
+
+    input = torch.randn((4, 25, 391, 1280), dtype=torch.float32)
+    heatmap = model.interpret(input)
+    print('heatmap', heatmap.shape)
+
+    heatmap_cameras = model.cameras(input)
+    print('heatmap_cameras', heatmap_cameras.shape)
+    exit()
+
+    channel_importance = model.channel_importance(input)
+    print('channel_importance', channel_importance.shape)
+    exit()
+
+    from thop import profile
+    model.eval()
+    with torch.no_grad():
+        macs, params = profile(model, inputs=(input, ), verbose=0)
+        print(f'GMACs = {macs / 10 ** 9:.4f}')
+        print(f'params(M) = {params / 10 ** 6:.4f}')
+    exit()
+
+    # test raw input
+    model = EEGResNet18Raw(pretrained=False, initial_bn=True)
+    print(model)
+
+    input = torch.randn((4, 1, 25, 1280), dtype=torch.float32)
+
+    from thop import profile
+    model.eval()
+    with torch.no_grad():
+        macs, params = profile(model, inputs=(input,), verbose=0)
+        print(f'GMACs = {macs / 10 ** 9}')
+        print(f'params(M) = {params / 10 ** 6}')
